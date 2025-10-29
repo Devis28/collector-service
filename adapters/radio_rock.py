4498d84624bbba1c642c9e62ac7eba7a960cf60f
Devis28 <davidteplan5@gmail.com>
2025-10-29 01:39:39 +0100

    add radio_rock.py, add thread for each radio

--- adapters/radio_rock.py
+++ adapters/radio_rock.py
@@ -41,37 +41,38 @@ def get_current_song():
         }
 
 async def get_current_listeners(session_id=None):
-    uri = LISTENERS_WS
+    uri = "wss://rock-server.fly.dev/ws/push/listenership"
     listeners_data = None
+    last_data = None
     async with websockets.connect(uri) as websocket:
         try:
-            listeners_msg = None
-            # Čakáme max 10x3s na listeners_update správu
-            for _ in range(10):
+            # Slučka - 12 pokusov po 2.5 sekundy (spolu max 30 sekúnd na listeners)
+            for _ in range(12):
                 try:
-                    raw = await asyncio.wait_for(websocket.recv(), timeout=3)
+                    raw = await asyncio.wait_for(websocket.recv(), timeout=2.5)
                 except asyncio.TimeoutError:
                     continue
                 data = json.loads(raw)
+                last_data = data
                 if data.get("type") == "listeners_update":
-                    listeners_msg = data
+                    listeners = data.get("listeners")
+                    last_update = data.get("last_update")
+                    raw_valid = listeners is not None and last_update is not None
+                    listeners_data = {
+                        "listeners": listeners,
+                        "last_update": last_update,
+                        "recorded_at": datetime.now(ZoneInfo("Europe/Bratislava")).strftime("%d.%m.%Y %H:%M:%S"),
+                        "raw_valid": raw_valid,
+                        "song_session_id": session_id
+                    }
                     break
-            if listeners_msg:
-                listeners = listeners_msg.get("listeners")
-                last_update = listeners_msg.get("last_update")
-                raw_valid = listeners is not None and last_update is not None
-                listeners_data = {
-                    "listeners": listeners,
-                    "last_update": last_update,
-                    "recorded_at": datetime.now(ZoneInfo("Europe/Bratislava")).strftime("%d.%m.%Y %H:%M:%S"),
-                    "raw_valid": raw_valid,
-                    "song_session_id": session_id
-                }
-            else:
+            # Je listeners správa?
+            if not listeners_data:
                 listeners_data = {
                     "recorded_at": datetime.now(ZoneInfo("Europe/Bratislava")).strftime("%d.%m.%Y %H:%M:%S"),
                     "raw_valid": False,
-                    "song_session_id": session_id
+                    "song_session_id": session_id,
+                    "debug_ws_last_message": last_data  # na debug, môžeš mazať v ostrej prevádzke
                 }
         except Exception:
             listeners_data = {
